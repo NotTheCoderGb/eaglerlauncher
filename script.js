11b1a63cb25200ef97be52ab26b4140dda3f73e3
Notthecoder <84812950+NotTheCoderGb@users.noreply.github.com>
2023-09-21 12:05:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+// Add an event listener to the button
+document.getElementById("switchToHelp").addEventListener("click", function() {
+    // Redirect to help.html when the button is clicked
+    window.location.href = "1.5.2/1.5.2.html";
+});
+
+document.getElementById("switchToother").addEventListener("click", function() {
+    // Redirect to help.html when the button is clicked
+    window.location.href = "1.8.8/1.8.8.html";
+});
+document.getElementById("resent").addEventListener("click", function() {
+    // Redirect to help.html when the button is clicked
+    window.location.href = "resent/index.html";
+});
