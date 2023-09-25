147ce5c21c43b1f24d41395f1b09bc229a051e4b
notcoder <gaemsnahni3@gmail.com>
2023-09-25 11:50:29 -0400

    s

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ document.getElementById("launchButton").addEventListener("click", function() {
     // Redirect to the corresponding HTML page based on the selected version
     if (selectedVersion === "1.5.2") {
         window.location.href = "1.5.2/1.5.2.html";
-    } else if (selectedVersion === "1.8.8") {
-        window.location.href = "1.8.8/1.8.8.html";
+    } else if (selectedVersion === "reborn") {
+        window.location.href = "reborn/1.8.8.html";
     } else if (selectedVersion === "resent") {
         window.location.href = "resent/index.html";
     }
