9f042762fdf7b1b391c302ed07a294411078cfe0
notcoder <gaemsnahni3@gmail.com>
2023-09-25 11:44:45 -0400

    visual overhaul

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // script.js
 
-// Add an event listener to the button
-document.getElementById("switchToHelp").addEventListener("click", function() {
-    // Redirect to help.html when the button is clicked
-    window.location.href = "1.5.2/1.5.2.html";
-});
-
-document.getElementById("switchToother").addEventListener("click", function() {
-    // Redirect to help.html when the button is clicked
-    window.location.href = "1.8.8/1.8.8.html";
-});
-document.getElementById("resent").addEventListener("click", function() {
-    // Redirect to help.html when the button is clicked
-    window.location.href = "resent/index.html";
+// Add an event listener to the launch button
+document.getElementById("launchButton").addEventListener("click", function() {
+    // Get the selected version from the select element
+    var selectedVersion = document.getElementById("minecraftVersion").value;
+    
+    // Redirect to the corresponding HTML page based on the selected version
+    if (selectedVersion === "1.5.2") {
+        window.location.href = "1.5.2/1.5.2.html";
+    } else if (selectedVersion === "1.8.8") {
+        window.location.href = "1.8.8/1.8.8.html";
+    } else if (selectedVersion === "resent") {
+        window.location.href = "resent/index.html";
+    }
 });
